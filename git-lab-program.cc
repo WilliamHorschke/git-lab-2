361ff6ad4345f65cbda97bd7e8160224b3978b6e
William Horschke <wh477117@ohio.edu>
2021-01-27 19:15:13 -0500

    Created and modified git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: William Horschke
+ *        Date: 1/27/2021
+ * Description: Program for Lab 2 github lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
